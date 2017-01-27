4760adc0645facab567c39e930efd066feb00b11
Eric Forsell <efor202@gmail.com>
2017-01-27 18:07:43 -0500

    (feat) Issue setting up database

---
+++ database/config.js
@@ -0,0 +1,3 @@
+//usually holds routing and middleware
+var Sequelize = require('sequelize');
+var db = require('db.js');
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
+var db = require('../database/db.js');
 var app = express();
 app.use(express.static(__dirname + "/../client"));
 app.use(bodyParser.json());
