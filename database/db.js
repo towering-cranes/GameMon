170442f86c014fefc499405501a06bd03bcdaca2
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-27 19:51:15 -0800

    Work on database

--- database/db.js
+++ database/db.js
@@ -5,64 +5,67 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('gamemon', 'root', '');
 
 var User = db.define('User', {
-  username: Sequelize.STRING,
+  username: {type: Sequelize.STRING},
   password: Sequelize.STRING
 });
 
 var Game = db.define('Game', {
   giantBombId: Sequelize.INTEGER,
-  title: {type: Sequelize.STRING, unique: true},
+  title: {type: Sequelize.STRING},
   aliases: Sequelize.STRING,
   image: Sequelize.STRING,
-  images: Sequelize.STRING,
-  releaseYear: Sequelize.STRING,
+  releaseDate: Sequelize.DATE,
   publishers: Sequelize.STRING,
   developers: Sequelize.STRING,
   summary: Sequelize.STRING,
-  fullDescription: Sequelize.STRING,
   similarGames: Sequelize.STRING,
   videos: Sequelize.STRING
 });
 
 
 var Franchise = db.define('Franchise', {
-  franchise: {type: Sequelize.STRING, unique: true}
+  franchise: {type: Sequelize.STRING}
 });
 
 var Platform = db.define('Platform', {
-  platform: {type: Sequelize.STRING, unique: true}
+  platform: {type: Sequelize.STRING}
 });
 
 var Genre = db.define('Genre', {
-  genre: {type: Sequelize.STRING, unique: true}
+  genre: {type: Sequelize.STRING}
 });
+
+var GameLibrary = db.define('GameLibrary', {});
+var GamePlatform = db.define('GamePlatform', {});;
+var GameGenre = db.define('GameGenre', {});;
 
 //creating a GameLibrary join table to holding users and games
 User.belongsToMany(Game, {through: 'GameLibrary'});
 Game.belongsToMany(User, {through: 'GameLibrary'});
 
-
 //one to many relationship between franchise and game
-// Game.hasOne(Franchise);
-Franchise.belongsTo(Game);
-// Franchise.hasMany(Game);
+Franchise.hasMany(Game);
+Game.belongsTo(Franchise);
 
 //create a join table for games and platforms
 
-// Game.belongsToMany(Platform, {through: 'GamePlatform'});
-// Platform.belongsToMany(Game, {through: 'GamePlatform'});
+Game.belongsToMany(Platform, {through: 'GamePlatform', });
+Platform.belongsToMany(Game, {through: 'GamePlatform'});
 
 //create a join table for games and genres
 
-// Game.belongsToMany(Genre, {through: 'GameGenre'});
-// Genre.belongsToMany(Game, {through: 'GameGenre'});
+Game.belongsToMany(Genre, {through: 'GameGenre'});
+Genre.belongsToMany(Game, {through: 'GameGenre'});
 
 //creates tables in mysql if they don't exist
-User.sync({force: true});
-Game.sync({force: true});
-Franchise.sync({force: true});
-Platform.sync({force: true});
-Genre.sync({force: true});
+User.sync();
+Game.sync();
+Franchise.sync();
+Platform.sync();
+Genre.sync();
+GameLibrary.sync();
+GamePlatform.sync();
+GameGenre.sync();
 
 //export them for use
 
@@ -71,5 +74,6 @@ exports.Game = Game;
 exports.Franchise = Franchise;
 exports.Platform = Platform;
 exports.Genre = Genre;
-
-
+exports.GameLibrary = GameLibrary;
+exports.GamePlatform = GamePlatform;
+exports.GameGenre = GameGenre;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,53 @@ var db = require('../database/db.js');
 var app = express();
 app.use(express.static(__dirname + "/../client"));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
+
+app.post('/users', function(req, res) {
+  var newUser = req.body;
+  var makeUser = function(username, password) {
+    var user = db.User.build({
+      username: username,
+      password: password
+    });
+    user.save().then(function(user) {
+      console.log('user created: ', user);
+      res.send(user);
+    });
+  }
+  makeUser(newUser.username, newUser.password);
+});
+
+app.post('/games', function(req, res) {
+  var newGame = req.body;
+  var addGame = function(gameObj, username) {
+    db.User.findOne({
+      where: {
+        username: username
+      }
+    }).then(function(user) {
+      var game = gameObj.results;
+      var newGame = db.Game.build({
+        giantBombId: game.id,
+        title: game.name,
+        aliases: game.aliases, // string
+        image: game.image.super_url,
+        releaseDate: game.original_release_date,
+        publishers: JSON.stringify(game.publishers[0]), // array of objects
+        developers: JSON.stringify(game.developers[0]), // array of objects
+        summary: game.deck,
+        similarGames: JSON.stringify(game.similar_games[0]), // array of objects
+        videos: JSON.stringify(game.videos[0].api_detail_url) // array of objects
+      });
+      newGame.save().then(function(game) {
+        user.addGame(game);
+        game.addUser(user);
+        res.send(game);
+      });
+    })
+  };
+  addGame(newGame, 'test');
+});
 
 var server = app.listen(port, function() {
   console.log('Running on port: ', port);
