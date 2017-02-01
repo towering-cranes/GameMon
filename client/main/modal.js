bc79e483762f2070b430d2bf760f346e9fa5d511
kjng <pianistkevinjang@gmail.com>
2017-02-01 14:22:54 -0800

    Create factory to pass selected game

--- client/main/modal.js
+++ client/main/modal.js
@@ -1,9 +1,11 @@
 // controller for modal
-var app = angular.module('gameMon.modal', ['ui.materialize']);
-app.controller('ModalController', function($scope){
+var app = angular.module('gameMon.modal', ['ui.materialize', 'gameMon.selectedGame']);
+app.controller('ModalController', function($scope, selectedGame){
   $scope.data = {};
   $scope.data.similarGames = {};
   for(var i = 0; i < 5; i++){
     $scope.data.similarGames[i] = 'Hello';
   };
+
+  console.log(selectedGame);
 });
