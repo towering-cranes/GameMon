070da4ce50eb7eb7b380a54635fbae556f1a262f
kjng <pianistkevinjang@gmail.com>
2017-02-01 12:49:00 -0800

    Revert modal work

--- client/main/modal.js
+++ client/main/modal.js
@@ -1,6 +1,6 @@
 // controller for modal
-  var app = angular.module('gameMon.modal', ['ui.materialize', 'gameMon.gameCollection']);
-app.controller('ModalController', function($scope, userCollectionData){
+var app = angular.module('gameMon.modal', ['ui.materialize']);
+app.controller('ModalController', function($scope){
   $scope.data = {};
   $scope.data.similarGames = {};
   for(var i = 0; i < 5; i++){
