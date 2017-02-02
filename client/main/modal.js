5cc86bc72269436ba4a5b4d891da745d7f7fb3af
kjng <pianistkevinjang@gmail.com>
2017-02-02 10:23:55 -0800

    Refactor selectedGame to allow modal from both collection and search; display modal from search

--- client/main/modal.js
+++ client/main/modal.js
@@ -1,11 +1,27 @@
 // controller for modal
-var app = angular.module('gameMon.modal', ['ui.materialize', 'gameMon.selectedGame']);
+var app = angular.module('gameMon.modal', ['ui.materialize', 'gameMon.selectedGame', 'gameMon.search']);
 
-app.controller('ModalController', ['$scope', 'SelectedGame', '$rootScope', function($scope, SelectedGame, $rootScope) {
-  $scope.data = SelectedGame.currentGame;
-  $scope.similarGames;
-  $rootScope.$on('gameChange', function(event, game) {
+app.controller('ModalController', ['$scope', 'SelectedGame', '$rootScope', 'giantBomb', function($scope, SelectedGame, $rootScope, giantBomb) {
+  $scope.data = {};
+  $scope.similarGames = [];
+
+  // Game from search is different than collection
+  $rootScope.$on('gameChangeSearch', function(event, game) {
     $scope.data = game;
-    $scope.similarGames = game.similar_games;
+    $scope.similarGames = [];
+    $scope.data.image = game.image.small_url;
+    giantBomb.searchById(game.id, function(response) {
+      var game = response.data;
+      $scope.similarGames = game.similar_games;
+    });
+  });
+
+  $rootScope.$on('gameChangeCollection', function(event, game) {
+    $scope.data = game;
+    $scope.similarGames = [];
+    // Set up same format as giantbomb results...
+    $scope.data.name = game.title;
+    $scope.data.deck = game.summary;
+    $scope.similarGames = game.similarGames;
   });
 }]);
